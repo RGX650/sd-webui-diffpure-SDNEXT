3d4ab683b17bd5936f1d9dfc7aec40878104e35a
ZRGX <139933536+RGX650@users.noreply.github.com>
2024-01-13 11:02:27 +0100

    Update install.py

--- install.py
+++ install.py
@@ -1,26 +1,12 @@
-import os
-import launch
-import git
+# Read LICENSE for usage terms.
 
-# Get the path to the requirements.txt file
+import launch
+import os
+
 req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
 
-# Open the requirements.txt file and process each line
 with open(req_file) as file:
     for lib in file:
         lib = lib.strip()
-
-        # Check if the package is from a GitHub repository
-        if lib.startswith("git+"):
-            # Install the GitHub repository package
-            git_repo_url = lib[len("git+"):]
-            repo_name = os.path.basename(git_repo_url.rstrip(".git"))
-            install_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), repo_name)
-            
-            if not os.path.exists(install_path):
-                git.Repo.clone_from(git_repo_url, install_path)
-
-            launch.run_pip(f'install -e {install_path}', f"clear object requirement: {lib}")
-        elif not launch.is_installed(lib):
-            # Install other packages from PyPI if not already installed
-            launch.run_pip(f'install {lib}', f"clear object requirement: {lib}")
+        if not launch.is_installed(lib):
+            launch.run_pip(f"install {lib}", f"DiffPure requirement: {lib}")
